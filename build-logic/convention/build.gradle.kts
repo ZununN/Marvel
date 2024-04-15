7a59799736b8db20e632cdfd86d7c533648cedc5
Нурахмет Зунун <nzunun@n1broker.kz>
2024-04-15 15:55:37 +0500

    initial

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "kz.zunun.marvel.buildlogic"
---
+++ login/src/main/java/kz/zunun/login/Login.kt
@@ -0,0 +1,16 @@
+package kz.zunun.login
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+
+
+@Composable
+fun Login() {
+
+    Box(contentAlignment = Alignment.Center) {
+
+        Text(text = "login")
+    }
+}
